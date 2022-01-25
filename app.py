ee1c7fd91e03e438c036fdc87b7e01a4e7b02f82
chirathlv <72639221+chirathlv@users.noreply.github.com>
2022-01-25 20:01:18 +1100

    filtering section implemented and code restructed

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ from dotenv import load_dotenv
 import os
 import numpy as np
 
-#--------------------------#
-# APP CONFIG
+#-----------APP CONFIG---------------#
 APP_NAME = 'Churn Prediction App'
 st.set_page_config(page_title=APP_NAME,
                    page_icon=':bar_chart:', 
@@ -20,7 +19,7 @@ st.set_page_config(page_title=APP_NAME,
 st.title(APP_NAME)
 st.markdown('This app predicts for a given customer whether he will leave the company or staying')
 
-#--------------------------#
+#------------Mapbox API--------------#
 
 # Read the Mapbox API key
 load_dotenv()
@@ -29,122 +28,165 @@ map_box_api = os.getenv("mapbox")
 # Set the mapbox access token
 px.set_mapbox_access_token(map_box_api)
 
-#--------------------------#
+#------------Helper Functions--------------#
 
-# Helper Functions
-placeholder = st.empty()
-status = st.empty()
-def render(df):
-    status.info('Your Dataset is Uploaded. Hit Predict!')
-    #placeholder.dataframe(df)
-    placeholder.write(df)
-    placeholder.caption(f"Rows: {df.shape[0]} | Columns: {df.shape[1]}")
+if 'key' not in st.session_state:
+    st.session_state['key'] = 'begin'
 
-#--------------------------#
+def save_data(df):
+    open('__df__.csv', 'w').write(df.to_csv(index=False))
 
-# SIDEBAR
+# Draw Map
+def churn_map(df):
+    churn_map = px.scatter_mapbox(
+            df,
+            lat='lat',
+            lon='long',
+            color='prediction',
+            size='monthly_charges',
+            title='<b>Customer Churn Map</b>',
+            color_continuous_scale=px.colors.sequential.Rainbow,
+            size_max=25,
+            zoom=8)
+
+    churn_map.update_traces(marker=dict(size=np.where(df['prediction'] == 'Churn Customer', 25, 8)),
+                                    selector=dict(mode='markers'))
+
+    st.plotly_chart(churn_map, use_container_width=True)
+
+#----------SIDEBAR----------------#
+
+# Sidebar Header
+st.sidebar.header('Upload your EXCEL / CSV data')
 
 # Loading Dataset Module
-with st.sidebar.header('Upload your EXCEL / CSV data'):
-    upload_data = st.sidebar.file_uploader("Upload your customers data here..", type=['csv', 'xlsx'])
-
-
-#--------------------------#
-
-# BODY
+upload_data = st.sidebar.file_uploader("Upload your customers data here..", type=['csv', 'xlsx'])
 
 # Display Uploaded Dataset
 st.subheader('Customer Data')
 
-if upload_data is not None:
+def get_data():
+    if st.session_state['key'] == 'begin':
+        if upload_data is not None:
+            # Read the dataset
+            ext = upload_data.name.rsplit('.', 1)[1]
+            if ext == 'csv':
+                df = pd.read_csv(upload_data, 
+                                    encoding='UTF-8',
+                                    index_col=False)
+            elif ext == 'xlsx':
+                df = pd.read_excel(upload_data,
+                                        engine='openpyxl', 
+                                        sheet_name=0, 
+                                        encoding='UTF-8',
+                                        index_col=False)
+            else:
+                return None
 
-    # Read the dataset
-    ext = upload_data.name.rsplit('.', 1)[1]
-    if ext == 'csv':
-        telco_df = pd.read_csv(upload_data, 
-                            encoding='UTF-8',
-                            index_col=False)
-    elif ext == 'xlsx':
-        telco_df = pd.read_excel(upload_data,
-                                engine='openpyxl', 
-                                sheet_name=0, 
-                                encoding='UTF-8',
-                                index_col=False)
-    else:
-        telco_df = None
-
-    if (telco_df is not None):
-        if not telco_df.empty:
-            render(telco_df)
+            if (df is not None):
+                if not df.empty:
+                    st.caption(f"Rows: {df.shape[0]} | Columns: {df.shape[1]}")
+                    st.info('Your Dataset is Uploaded. Hit Predict!')
+                    return df
+                else:
+                    st.info('Your Dataset is empty, please check if you have data in the file before uploading!')
+                    return None
+            else:
+                st.info('Your Dataset has a problem, please check before uploading!')
+                return None
         else:
-            st.info('Your Dataset is empty, please check if you have data in the file before uploading!')
+            st.info('Upload your CSV file from the sidebar section')
+            return None
+    
     else:
-        st.info('Your Dataset has a problem, please check before uploading!')
-else:
-    st.info('Upload your CSV file from the sidebar section')
+        df = pd.read_csv('__df__.csv', 
+                        encoding='UTF-8',
+                        index_col=False)
+        return df
 
+# Load the data
+telco_df = get_data()
+
+#--------------------------#
 
 # Machine Learning Prediction
-if st.button("Predict"):
+if st.session_state.key == 'begin':
+    if st.button("Predict"):
+        if upload_data is not None:
+            st.session_state.key = 'predict'
+            predictions = []
+            my_bar = st.progress(0)
+            for i in range(telco_df.shape[0]):
+                data = telco_df[['city',
+                            'gender',
+                            'senior_citizen',
+                            'partner',
+                            'dependents',
+                            'phone_service',
+                            'multiple_lines',
+                            'internet_service',
+                            'online_security',
+                            'online_backup',
+                            'device_protection',
+                            'tech_support',
+                            'streaming_tv',
+                            'streaming_movies',
+                            'contract',
+                            'paperless_billing',
+                            'payment_method',
+                            'monthly_charges',
+                            'total_charges',
+                            'tenure',
+                            'lat',
+                            'long',
+                            'zip']][i:i+1].to_json(orient='records')
 
-    # Change Status
-    with status.container():
-        status.empty()
+                res = requests.post("http://ec2-3-25-148-140.ap-southeast-2.compute.amazonaws.com:8000/predict/", json=json.loads(data[1:-1]))
+                prediction = json.loads(res.text)
+                predictions.append(prediction['prediction'][0])
+                my_bar.progress(i+1)
 
-    predictions = []
-    my_bar = st.progress(0)
-    for i in range(telco_df.shape[0]):
-        data = telco_df[['city',
-                    'gender',
-                    'senior_citizen',
-                    'partner',
-                    'dependents',
-                    'phone_service',
-                    'multiple_lines',
-                    'internet_service',
-                    'online_security',
-                    'online_backup',
-                    'device_protection',
-                    'tech_support',
-                    'streaming_tv',
-                    'streaming_movies',
-                    'contract',
-                    'paperless_billing',
-                    'payment_method',
-                    'monthly_charges',
-                    'total_charges',
-                    'tenure',
-                    'lat',
-                    'long',
-                    'zip']][i:i+1].to_json(orient='records')
+            #st.write(pd.DataFrame({'Prediction' : predictions}))
+            prediction_df = pd.DataFrame({'prediction' : predictions})
+            telco_df = pd.concat([prediction_df, telco_df], axis=1)
+            save_data(telco_df)
 
-        res = requests.post("http://127.0.0.1:8000/predict/", json=json.loads(data[1:-1]))
-        prediction = json.loads(res.text)
-        predictions.append(prediction['prediction'][0])
-        my_bar.progress(i+1)
+if st.session_state.key == 'predict':
 
-    #st.write(pd.DataFrame({'Prediction' : predictions}))
-    prediction_df = pd.DataFrame({'prediction' : predictions})
-    telco_df = pd.concat([prediction_df, telco_df], axis=1)
-
-    with placeholder.container():
-        # Update the telco_df
-        st.write(telco_df)
-
-
-    customers_map_plot = px.scatter_mapbox(
-        telco_df,
-        lat='lat',
-        lon='long',
-        color='prediction',
-        size='monthly_charges',
-        title='<b>Customer Churn Map</b>',
-        color_continuous_scale=px.colors.sequential.Rainbow,
-        size_max=25,
-        zoom=8
+    # Churn select
+    churn = st.sidebar.multiselect(
+        "Churn",
+        options=telco_df['prediction'].unique(),
+        default=telco_df['prediction'].unique()
+    )
+    
+    # Gender select
+    gender = st.sidebar.multiselect(
+        "Gender",
+        options=telco_df['gender'].unique(),
+        default=telco_df['gender'].unique()
     )
 
-    customers_map_plot.update_traces(marker=dict(size=np.where(telco_df['prediction'] == 'Churn Customer', 25, 8)),
-                                    selector=dict(mode='markers'))
+    # Contract select
+    contract = st.sidebar.multiselect(
+        "Contract",
+        options=telco_df['contract'].unique(),
+        default=telco_df['contract'].unique()
+    )
 
-    st.plotly_chart(customers_map_plot, use_container_width=True)
+    # City select
+    city = st.sidebar.multiselect(
+        "City",
+        options=telco_df['city'].unique(),
+        default=telco_df['city'].unique()
+    )
+
+    telco_df_query = telco_df.query(
+        'gender == @gender & prediction == @churn & contract == @contract & city == @city'
+    )
+
+    telco_df = telco_df_query.reset_index(drop=True)
+    st.dataframe(telco_df)
+
+    # Customers Location MAP
+    churn_map(telco_df)
